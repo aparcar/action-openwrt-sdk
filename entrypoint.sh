59afc90d0a7574e8f20d2e793a51051aa1321a33
Paul Spooren <mail@aparcar.org>
2020-07-10 09:24:16 -1000

    add FEEDPATH and compile single packages
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,31 @@
 #!/bin/sh
 
-set -e
+set -ex
 
-FEEDNAME="${FEED_NAME:-action}"
+FEEDNAME="${FEEDNAME:-action}"
+FEEDPATH="${FEEDPATH:-.}"
 IGNORE_ERRORS="${IGNORE_ERRORS:-n m}"
 
 cd /home/build/openwrt/
 
-echo "src-link $FEEDNAME $GITHUB_WORKSPACE/" >> feeds.conf.default
+FEEDNAME_SANI="$(echo $FEEDNAME | tr '-' '_')"
+mv feeds.conf.default feeds.conf
+echo "src-link $FEEDNAME_SANI $GITHUB_WORKSPACE/$FEEDPATH" >> feeds.conf
 
+echo -e "\n---- feeds.conf --- \n"
+cat feeds.conf
+echo -e "\n---- feeds.conf --- \n"
 
 ./scripts/feeds update -a
 ./scripts/feeds install -d y -p "$FEEDNAME" -a -f
 
 make defconfig
-make \
-	CONFIG_SIGNED_PACKAGES="$SIGNED_PACKAGES" \
-	IGNORE_ERRORS="$IGNORE_ERRORS" \
-	-j "$(nproc)"
+
+PACKAGES="$(./scripts/feeds list -r $FEEDNAME | awk '{ print $1 }')"
+for PACKAGE in $PACKAGES; do
+	echo "Building $PACKAGE"
+        make "package/$PACKAGE/compile" \
+                CONFIG_SIGNED_PACKAGES="$SIGNED_PACKAGES" \
+                IGNORE_ERRORS="$IGNORE_ERRORS" \
+                -j "$(nproc)"
+done
